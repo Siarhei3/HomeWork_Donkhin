1fc47ef208340aba70b8f0f80efaf28a73e0c0ca
Siarhei <serg_don90@mail.ru>
2023-02-20 12:28:45 +0100

    Html was added

---
+++ lesson_28.py
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>HTML HomeWork</title>
+</head>
+<body>
+
+	<h1>Добро пожаловать!</h1>
+	
+	<h3><font color="red">Lorem, ipsum.</font></h3>
+	
+	<p><u>Lorem, ipsum, dolor.</u> <s>Lorem ipsum, dolor sit.</s>Lorem ipsum dolor sit amet.<br /> 234<sub>2</sub>34 <br />34<sup>43</sup></p>
+	
+	<ul>
+		<li><font size="7">HTML</font></li>
+		<li>CSS</li>
+		<li>Python</li>
+	</ul>
+	
+	<ol>
+		<li>
+			Lorem, ipsum dolor sit amet.
+			<ul>
+				<li>sit</li>
+				<li>amet</li>
+				<li>ipsum</li>
+			</ul>
+		</li>
+		<li>Lorem ipsum dolor sit amet, consectetur.</li>
+		<li>Lorem ipsum dolor sit amet consectetur.</li>
+	</ol>
+	
+	<dl>
+		<dt>Lorem, ipsum.</dt>
+		<dd>Lorem ipsum, dolor sit amet.</dd>
+		<dt>Lorem, ipsum.</dt>
+		<dd>Lorem ipsum dolor sit, amet consectetur.</dd>
+		<dt>Lorem, ipsum.</dt>
+		<dd>Lorem ipsum dolor sit amet consectetur.</dd>
+	</dl>
+	
+	<img width="60%" src="image1.jpg" alt="Автомобиль" />
+
+	<p>Lorem ipsum dolor <br />sit amet consectetur adipisicing<br /> elit. Esse, ab. <strong>Velit adipisci facilis<br /> voluptate repudiandae impedit expedita<br /> quisquam, pariatur.</strong> Reprehenderit ipsa possimus esse ab.<br /> Ducimus maiores eius mollitia omnis sequi<br /> doloremque accusamus iusto similique magnam veritatis animi fugiat cum<br /> cupiditate ad explicabo assumenda dicta, repellat<br /> illum debitis sint aliquid corporis.</p>
+	
+	<hr />
+	
+	<p><em>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, rerum?</em> Iste explicabo deserunt quisquam blanditiis eum similique. Iure, ex facilis quae cumque ducimus reiciendis quidem maiores sunt possimus, laborum, quaerat cum id aspernatur neque veniam, eveniet? Voluptas incidunt, ullam! Maxime at perferendis, dicta quod blanditiis nesciunt natus iusto ullam libero!</p>
+	
+	<table width="90%" align="center" border="1">
+		<tr>
+			<td colspan="3">Ячейка 1</td>
+			<!--<td width="150">Ячейка 2</td>
+			<td>Ячейка 7</td>-->
+		</tr>
+		<tr>
+			<td align="center" height="80">Ячейка 3</td>
+			<td valign="top">Ячейка 4</td>
+			<td rowspan="2">Ячейка 8</td>
+		</tr>
+		<tr>
+			<td>Ячейка 5</td>
+			<td>Ячейка 6</td>
+			<!--<td>Ячейка 9</td>-->
+		</tr>
+	</table>
+	
+	<p>Источник: <a target="_blank" href="https://webref.ru/" title="HTML CSS Источник">WebReference</a></p>
+	
+</body>
+</html>
